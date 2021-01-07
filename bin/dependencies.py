04bab21941aa10dcf54a2cab44b8d4bdc51a06fc
Cyril Brulebois <cyril@debamax.com>
2021-01-07 14:12:17 +0100

    Add script to generate a dependency graph.
    It relies on data available on the uploader's machine, but could be
    adjusted to double check what's happening in the Sources file once all
    packages are accepted into the archive.
    
    This graph makes it possible to trigger source-only uploads in the right
    order once NEW has been cleared.
    
    The script (ab)uses the fact the relevant packages are following the
    same naming convention (binary := source + '-dev'). It could easily be
    made smarter, but that's probably not required at this stage.

---
+++ bin/dependencies.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+#
+# Do interesting things with dependencies.
+
+import os
+import re
+import sys
+
+from debian import deb822
+
+
+def build_dependencies(upload_dir):
+    os.chdir(upload_dir)
+    existing_dsc = [x for x in os.listdir('.')
+                    if x.endswith('.dsc')]
+    uploaded_dsc = [x for x in existing_dsc
+                    if os.path.exists(re.sub(r'\.dsc$', '_amd64.ftp-master.upload', x))]
+
+    sources = []
+    build_deps = []
+    for dsc in uploaded_dsc:
+        # Should be a single paragraph, but...
+        for pkg in deb822.Dsc.iter_paragraphs(open(dsc)):
+            # print(pkg['Source'])
+            # print(pkg['Build-Depends'])
+            # print()
+            sources.append(pkg['Source'])
+            for bd in pkg['Build-Depends'].split(', '):
+                bd = re.sub(r' \(.*\)', '', bd)
+                #print(pkg['Source'], '->', bd)
+                build_deps.append((pkg['Source'], bd))
+
+    accepted = [
+        "golang-github-oschwald-geoip2-golang",
+        "golang-github-oschwald-maxminddb-golang",
+        "golang-github-antonmedv-expr",
+        "golang-github-go-playground-locales",
+        "golang-github-enescakir-emoji",
+        "golang-github-go-playground-assert-v2",
+        "golang-github-appleboy-gofight",
+        "golang-github-go-playground-validator-v10",
+        "golang-github-jamiealquiza-tachymeter",
+        "golang-github-leodido-go-urn",
+        "golang-github-logrusorgru-grokky",
+        "golang-github-mohae-deepcopy",
+        "golang-github-go-playground-universal-translator",
+    ]
+    binaries = ['%s-dev' % x for x in sources]
+    print('digraph {')
+    print('  rankdir=LR;')
+    print('  node[shape=box];')
+    for (x, y) in build_deps:
+        if y in binaries:
+            print('  "%s" -> "%s"' % (x, re.sub(r'-dev$', '', y)))
+    for x in sources:
+        if x in accepted:
+            print('  "%s" [fillcolor=green,style=filled];' % x)
+    print('}')
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("E: Usage: %s path/with/uploads" % sys.argv[0])
+        sys.exit(1)
+    build_dependencies(sys.argv[1])
